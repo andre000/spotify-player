67a71d6559629ffbece29176f58e5c178c81db7b
Andre <andre@dev-next.com>
2018-12-26 16:14:01 -0200

    :package: Configuring Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                { targets: '> 5% in BR' },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+};
